9c7169ba4b1149946fc420acf65aed6cae63110c
almudenaris <almudenaris@gmail.com>
2014-03-28 16:21:43 +0100

    new ontology and documentation

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var ToC =
+  "<nav role='navigation' class='toc'>" +    
+    "<ol class='ol-list'>";
+
+var newLine, el, title, link;
+
+$("article h2").each(function() {
+
+  el = $(this);
+  title = el.text();
+  link = "#" + el.attr("id");
+
+  newLine =
+    "<li>" +
+      "<a href='" + link + "'>" +
+        title +
+      "</a>" +
+    "</li>";
+
+  ToC += newLine;
+
+});
+
+ToC +=
+   "</ol>" +
+  "</nav>";
+
+$("#toc").append(ToC);
